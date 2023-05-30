e4480a66586215c4c0b4ca739049f2ac98232fc8
m02-253 <mossan0917@gmail.com>
2023-05-30 14:42:06 +0900

    stones are reversed correctly

--- reversi/scripts/index.js
+++ reversi/scripts/index.js
@@ -1,3 +1,4 @@
+const PLAYER_STONE = 1  // 1: 白、 2: 黒
 const FIELD_SIZE_VERTICAL = 8
 const FIELD_SIZE_HORIZONTAL = 8
 const INIT_WHITE_LOCATIONS = [
@@ -45,7 +46,6 @@ class Reversi {
   }
 
   setStone(playerNum, loc) {
-    console.assert(loc instanceof Array)
     console.assert(0 <= loc.vertical < FIELD_SIZE_VERTICAL)
     console.assert(0 <= loc.horizontal < FIELD_SIZE_HORIZONTAL)
     console.assert(playerNum === 1 || playerNum === 2)
@@ -69,7 +69,7 @@ class Reversi {
       }
     }
     // 左方向に走査
-    for (let h = this.latestStone.loc.horizontal + 1; h >= 0; h--) {
+    for (let h = this.latestStone.loc.horizontal - 1; h >= 0; h--) {
       const searchedStone = this.stage[this.latestStone.loc.vertical][h]
       if (searchedStone === 0 || searchedStone === this.latestStone.playerNum) {
         break
@@ -79,7 +79,7 @@ class Reversi {
       }
     }
     // 下方向に走査
-    for (let v = this.latestStone.loc.vertical + 1; v < FIELD_SIZE_VERTICAL; v++) {
+    for (let v = this.latestStone.loc.vertical - 1; v < FIELD_SIZE_VERTICAL; v++) {
       const searchedStone = this.stage[v][this.latestStone.loc.horizontal]
       if (searchedStone === 0 || searchedStone === this.latestStone.playerNum) {
         break
@@ -99,63 +99,61 @@ class Reversi {
     }
 
     // 右下に走査
-    let offset = 0
+    let offset = 1
     while (this.latestStone.loc.vertical + offset < FIELD_SIZE_VERTICAL && this.latestStone.loc.horizontal + offset < FIELD_SIZE_HORIZONTAL) {
       const searchedStone = this.stage[this.latestStone.loc.vertical + offset][this.latestStone.loc.horizontal + offset]
       if (searchedStone === 0 || searchedStone === this.latestStone.playerNum) {
         break
       }
-      {
-        reversibleStoneLocs.push({
-          vertical: this.latestStone.loc.vertical + offset,
-          horizontal: this.latestStone.loc.horizontal + offset
-        })
-      }
+      reversibleStoneLocs.push({
+        vertical: this.latestStone.loc.vertical + offset,
+        horizontal: this.latestStone.loc.horizontal + offset
+      })
       offset++
     }
     // 左上に走査
-    offset = 0
+    offset = 1
     while (this.latestStone.loc.vertical - offset >= 0 && this.latestStone.loc.horizontal - offset >= 0) {
       const searchedStone = this.stage[this.latestStone.loc.vertical - offset][this.latestStone.loc.horizontal - offset]
       if (searchedStone === 0 || searchedStone === this.latestStone.playerNum) {
         break
       }
-      {
-        reversibleStoneLocs.push({
-          vertical: this.latestStone.loc.vertical - offset,
-          horizontal: this.latestStone.loc.horizontal - offset
-        })
-      }
+
+      reversibleStoneLocs.push({
+        vertical: this.latestStone.loc.vertical - offset,
+        horizontal: this.latestStone.loc.horizontal - offset
+      })
+
       offset++
     }
     // 左下に走査
-    offset = 0
+    offset = 1
     while (this.latestStone.loc.vertical + offset < FIELD_SIZE_VERTICAL && this.latestStone.loc.horizontal - offset >= 0) {
       const searchedStone = this.stage[this.latestStone.loc.vertical + offset][this.latestStone.loc.horizontal - offset]
       if (searchedStone === 0 || searchedStone === this.latestStone.playerNum) {
         break
       }
-      {
-        reversibleStoneLocs.push({
-          vertical: this.latestStone.loc.vertical + offset,
-          horizontal: this.latestStone.loc.horizontal - offset
-        })
-      }
+
+      reversibleStoneLocs.push({
+        vertical: this.latestStone.loc.vertical + offset,
+        horizontal: this.latestStone.loc.horizontal - offset
+      })
+
       offset++
     }
     // 右上に走査
-    offset = 0
+    offset = 1
     while (this.latestStone.loc.vertical - offset >= 0 && this.latestStone.loc.horizontal + offset < FIELD_SIZE_HORIZONTAL) {
       const searchedStone = this.stage[this.latestStone.loc.vertical - offset][this.latestStone.loc.horizontal + offset]
       if (searchedStone === 0 || searchedStone === this.latestStone.playerNum) {
         break
       }
-      {
-        reversibleStoneLocs.push({
-          vertical: this.latestStone.loc.vertical - offset,
-          horizontal: this.latestStone.loc.horizontal + offset
-        })
-      }
+
+      reversibleStoneLocs.push({
+        vertical: this.latestStone.loc.vertical - offset,
+        horizontal: this.latestStone.loc.horizontal + offset
+      })
+
       offset++
     }
 
@@ -164,21 +162,33 @@ class Reversi {
 
   reverse() {
     const reversibleStoneLocs = this.searchReversibleStoneLocs()
-    for (const loc of reversibleStoneLocs){
+    for (const loc of reversibleStoneLocs) {
       this.stage[loc.vertical][loc.horizontal] = this.latestStone.playerNum
     }
   }
 
-  renderStage(){
+  // DOM操作系
+  emptyCellRowContainer() {
     const cellRowContainer = document.getElementById('cell-row-container')
-    for (let v = 0; v < FIELD_SIZE_VERTICAL; v++)  {
+    cellRowContainer.innerText = ''
+  }
+
+  renderStage() {
+    const cellRowContainer = document.getElementById('cell-row-container')
+    for (let v = 0; v < FIELD_SIZE_VERTICAL; v++) {
       const cellRow = document.createElement('div')
       cellRow.className = 'flex'
       for (let h = 0; h < FIELD_SIZE_HORIZONTAL; h++) {
         const cell = document.createElement('div')
         cell.className = 'cell'
+        cell.onclick = () => {
+          this.setStone(PLAYER_STONE, {vertical: v, horizontal: h})
+          this.reverse()
+          this.emptyCellRowContainer()
+          this.renderStage()
+        }
         const stoneImg = document.createElement('img')
-        if (this.stage[v][h] === 1){
+        if (this.stage[v][h] === 1) {
           stoneImg.src = 'images/game_reversi_white.png'
         } else if (this.stage[v][h] === 2) {
           stoneImg.src = 'images/game_reversi_black.png'
@@ -191,8 +201,7 @@ class Reversi {
   }
 
 
-
-  mainLoop(){
+  mainLoop() {
 
   }
 }
