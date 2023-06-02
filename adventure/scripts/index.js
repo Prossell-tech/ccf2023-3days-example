9c431cb30d37ced00b87f2373766b5d0b3662210
m02-253 <mossan0917@gmail.com>
2023-06-02 17:12:10 +0900

    mario added

--- adventure/scripts/index.js
+++ adventure/scripts/index.js
@@ -1,7 +1,7 @@
+gsap.registerPlugin(ScrollTrigger)
+
 const ground = document.querySelector('.ground');
 const groundLandscapeContainer = document.querySelector('.ground .landscape-container');
-
-gsap.registerPlugin(ScrollTrigger)
 
 gsap.to(groundLandscapeContainer, {
   x: () => +(groundLandscapeContainer.clientWidth - ground.clientWidth),
@@ -10,6 +10,24 @@ gsap.to(groundLandscapeContainer, {
     trigger: '.ground',
     start: 'top top',
     end: () => `-=${groundLandscapeContainer.clientWidth - ground.clientWidth}`,  // landscapeContainerがgroundの外に出た瞬間に終わる
+    scrub: true,
+    pin: true,
+    anticipatePin: 1,
+    invalidateOnRefresh: true,
+    markers: true
+  },
+});
+
+const underground = document.querySelector('.underground');
+const undergroundLandscapeContainer = document.querySelector('.underground .landscape-container');
+
+gsap.to(undergroundLandscapeContainer, {
+  x: () => +(undergroundLandscapeContainer.clientWidth - underground.clientWidth),
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.underground',
+    start: 'top top',
+    end: () => `-=${undergroundLandscapeContainer.clientWidth - underground.clientWidth}`,  // landscapeContainerがgroundの外に出た瞬間に終わる
     scrub: true,
     pin: true,
     anticipatePin: 1,
